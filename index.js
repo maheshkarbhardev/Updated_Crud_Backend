89fd66041f61fed4ae8ff5e082e12fb0e3ac8b91
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2025-04-20 06:00:40 +0530

    server started

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const express=require('express');
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Backend Server Started At Port: 5000");
+});
