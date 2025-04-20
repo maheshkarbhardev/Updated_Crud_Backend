a482814e2b04fd00f2364c9e353ba604ece2fb33
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2025-04-20 07:59:28 +0530

    Get API Created

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.post("/api/post",(req,res)=>{
     const {firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate}=req.body;
 
-    //To format a date in DD-MM-YYYY
+    //To format a date in YYYY-MM-DD
     const formattedDate= new Date(birthDate).toISOString().split("T")[0];
 
     if(passwords !== confirmPasswords){
         return res.status(400).json({message:"password does not match."})
     }
 
+    //To get multiple values from checkbox , when you select multiple checkboxes.
     const interestsString=interests.join(",");
     
     const sqlInsert="INSERT INTO employee (firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
@@ -30,6 +31,20 @@ app.post("/api/post",(req,res)=>{
         res.status(200).json({message:"Data Inserted Successfully."})
     })
 })
+
+app.get("/api/get",(req,res)=>{
+    const sqlGet="SELECT * FROM employee";
+
+    db.query(sqlGet,(err,result)=>{
+        if(err){
+            console.log("Error:- ",err);
+            res.status(400).json({error:"Fail to fetch data."})            
+        }
+        res.send(result);
+    })
+})
+
+
 
 app.listen(5000, () => {
   console.log("Backend Server Started At Port: 5000");
