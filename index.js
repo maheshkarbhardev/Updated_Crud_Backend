60d28348ecd28d68d8f17963887bfac525b14e30
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2025-04-22 10:33:06 +0530

    basic changes

--- index.js
+++ index.js
@@ -1,138 +1,207 @@
 const express = require("express");
 const app = express();
-const cors=require('cors');
-const bodyParser=require('body-parser');
-const db=require('./database');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./database");
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api/post",(req,res)=>{
-    const {firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate}=req.body;
+app.post("/api/post", (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    passwords,
+    confirmPasswords,
+    age,
+    gender,
+    department,
+    interests,
+    birthDate,
+  } = req.body;
 
-    //To format a date in YYYY-MM-DD
-    const formattedDate= new Date(birthDate).toISOString().split("T")[0];
+  const formattedDate = new Date(birthDate).toISOString().split("T")[0];
 
-    if(passwords !== confirmPasswords){
-        return res.status(400).json({message:"password does not match."})
+  if (passwords !== confirmPasswords) {
+    return res.status(400).json({ message: "password does not match." });
+  }
+
+  const interestsString = interests.join(",");
+
+  const sqlInsert =
+    "INSERT INTO employee (firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+  db.query(
+    sqlInsert,
+    [
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      passwords,
+      confirmPasswords,
+      age,
+      gender,
+      department,
+      interestsString,
+      formattedDate,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log("Error:- ", err);
+        return res.status(400).json({ error: "Fail to insert data." });
+      }
+      res.status(200).json({ message: "Data Inserted Successfully." });
     }
+  );
+});
 
-    //To get multiple values from checkbox , when you select multiple checkboxes.
-    const interestsString=interests.join(",");
-    
-    const sqlInsert="INSERT INTO employee (firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+app.get("/api/get", (req, res) => {
+  const sqlGet = "SELECT * FROM employee";
 
-    db.query(sqlInsert,[firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interestsString, formattedDate],(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to insert data."})            
-        }
-        res.status(200).json({message:"Data Inserted Successfully."})
-    })
-})
-
-app.get("/api/get",(req,res)=>{
-    const sqlGet="SELECT * FROM employee";
-
-    db.query(sqlGet,(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to fetch data."})            
-        }
-        res.send(result);
-    })
-})
-
-app.delete("/api/delete/:id",(req,res)=>{
-    const {id}=req.params;
-    const sqlDelete="DELETE FROM employee WHERE id=?";
-
-    db.query(sqlDelete,id,(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to insert data."})            
-        }
-        res.status(200).json({message:"Data deleted Successfully."})
-    })
-})
-
-app.put("/api/update/:id",(req,res)=>{
-    const {id}=req.params;
-    const {firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interests, birthDate}=req.body;
-
-    const formattedDate= new Date(birthDate).toISOString().split("T")[0];
-
-    if(passwords !== confirmPasswords){
-        return res.status(400).json({message:"Passwords are not matching."})
+  db.query(sqlGet, (err, result) => {
+    if (err) {
+      console.log("Error:- ", err);
+      return res.status(400).json({ error: "Fail to fetch data." });
     }
+    res.send(result);
+  });
+});
 
-    const interetstString=Array.isArray(interests) ? interests.join(","):"";
+app.delete("/api/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlDelete = "DELETE FROM employee WHERE id=?";
 
-    const sqlUpdate="UPDATE employee SET firstName=? , lastName=? , email=? ,phoneNumber=? ,passwords=? ,confirmPasswords=? ,age=? ,gender=? ,department=? ,interests=? ,birthDate=? WHERE id=?";
-
-    db.query(sqlUpdate,[firstName, lastName, email, phoneNumber, passwords, confirmPasswords, age, gender, department, interetstString, formattedDate,id],(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to update data."})            
-        }
-
-        res.status(200).json({message:"Successfully data updated."})
-    })
-})
-
-app.get("/api/get/:id",(req,res)=>{
-    const {id}=req.params;
-    const sqlGet="SELECT * FROM employee WHERE id=?";
-
-    db.query(sqlGet,id,(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to fetch data."})            
-        }
-        res.send(result);
-    })
-})
-
-app.get("/api/search",(req,res)=>{
-    const {firstName,lastName,email,phoneNumber}=req.query;
-
-    const sqlSearch="SELECT * FROM employee WHERE firstName LIKE ? OR lastName LIKE ? OR email LIKE ? OR phoneNumber LIKE ?";
-
-    db.query(sqlSearch,[`%${firstName}%`,`%${lastName}%`,`%${email}%`,`%${phoneNumber}%`],(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to search data."})            
-        }
-        res.send(result);
-    })
-})
-
-app.get("/api/sort",(req,res)=>{
-    const {sortField,sortOrder}=req.query;
-
-    const allowedFields=['firstName','lastName','email'];
-    const allowedOrder=['ASC','DESC'];
-
-    if(!allowedFields.includes(sortField)){
-        return res.status(400).json({error:"Invalid sorting field."})
+  db.query(sqlDelete, id, (err, result) => {
+    if (err) {
+      console.log("Error:- ", err);
+      return res.status(400).json({ error: "Fail to delete data." });
     }
+    res.status(200).json({ message: "Data deleted Successfully." });
+  });
+});
 
-    sortOrder=sortOrder.toUpperCase();
-    if(!allowedOrder.includes(sortOrder)){
-        return res.status(400).json({error:"Invalid Sorting Order."})
+app.put("/api/update/:id", (req, res) => {
+  const { id } = req.params;
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    passwords,
+    confirmPasswords,
+    age,
+    gender,
+    department,
+    interests,
+    birthDate,
+  } = req.body;
+
+  const formattedDate = new Date(birthDate).toISOString().split("T")[0];
+
+  if (passwords !== confirmPasswords) {
+    return res.status(400).json({ message: "Passwords are not matching." });
+  }
+
+  const interestsString = Array.isArray(interests)
+    ? interests.join(",")
+    : "";
+
+  const sqlUpdate =
+    "UPDATE employee SET firstName=?, lastName=?, email=?, phoneNumber=?, passwords=?, confirmPasswords=?, age=?, gender=?, department=?, interests=?, birthDate=? WHERE id=?";
+
+  db.query(
+    sqlUpdate,
+    [
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      passwords,
+      confirmPasswords,
+      age,
+      gender,
+      department,
+      interestsString,
+      formattedDate,
+      id,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log("Error:- ", err);
+        return res.status(400).json({ error: "Fail to update data." });
+      }
+
+      res.status(200).json({ message: "Successfully data updated." });
     }
+  );
+});
 
-    const sqlSort=`SELECT * FROM employee ORDER BY ${sortField} ${sortOrder}`;
+app.get("/api/get/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlGet = "SELECT * FROM employee WHERE id=?";
 
-    db.query(sqlSort,(err,result)=>{
-        if(err){
-            console.log("Error:- ",err);
-            res.status(400).json({error:"Fail to sort"})            
-        }
-        res.send(result);
-    })
-})
+  db.query(sqlGet, id, (err, result) => {
+    if (err) {
+      console.log("Error:- ", err);
+      return res.status(400).json({ error: "Fail to fetch data." });
+    }
+    res.send(result);
+  });
+});
+
+app.get("/api/search", (req, res) => {
+  const { firstName, lastName, email, phoneNumber } = req.query;
+
+  const sqlSearch =
+    "SELECT * FROM employee WHERE firstName LIKE ? OR lastName LIKE ? OR email LIKE ? OR phoneNumber LIKE ?";
+
+  db.query(
+    sqlSearch,
+    [
+      `%${firstName}%`,
+      `%${lastName}%`,
+      `%${email}%`,
+      `%${phoneNumber}%`,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log("Error:- ", err);
+        return res.status(400).json({ error: "Fail to search data." });
+      }
+      res.send(result);
+    }
+  );
+});
+
+app.get("/api/sort", (req, res) => {
+  let { sortField, sortOrder } = req.query;
+
+  const allowedFields = ["firstName", "lastName", "email"];
+  const allowedOrder = ["ASC", "DESC"];
+
+  if (!allowedFields.includes(sortField)) {
+    return res.status(400).json({ error: "Invalid sorting field." });
+  }
+
+  sortOrder = sortOrder.toUpperCase();
+  if (!allowedOrder.includes(sortOrder)) {
+    return res.status(400).json({ error: "Invalid Sorting Order." });
+  }
+
+  const sqlSort = `SELECT * FROM employee ORDER BY ${sortField} ${sortOrder}`;
+
+  db.query(sqlSort, (err, result) => {
+    if (err) {
+      console.log("Error:- ", err);
+      return res.status(400).json({ error: "Fail to sort" });
+    }
+    res.send(result);
+  });
+});
 
 app.listen(5000, () => {
   console.log("Backend Server Started At Port: 5000");
